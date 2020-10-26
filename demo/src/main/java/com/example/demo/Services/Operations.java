9a08ffe8876cc1014a83cdc3446c27e3cc40a275
Dayvson <dayvsonwilkson@gmail.com>
2020-10-26 20:11:06 -0300

    feat: add operations service

---
+++ demo/src/main/java/com/example/demo/Services/Operations.java
@@ -0,0 +1,47 @@
+package com.example.demo.Services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class Operations {
+    
+	public String sum(double x, double y) {
+    try {        
+      return String.valueOf(x + y);
+    } catch (Exception e) {
+      return e.getMessage();
+    }
+  }
+
+  public String sub(double x, double y) {
+    try {
+      return String.valueOf(x - y);   
+    } catch (Exception e) {  
+      return e.getMessage();
+    }
+  }
+
+  public String mult(double x, double y) {
+    try {
+      return String.valueOf(x * y);
+    } catch (Exception e) {   
+      return e.getMessage();
+    }
+  }
+
+  public String div(double x, double y) {
+    try {
+      return String.valueOf(x / y);
+    } catch (Exception e) {   
+      return e.getMessage();
+    }
+  }
+
+  public String potencia(double x, double y) {
+    try {
+      return String.valueOf(Math.pow(x, y)); 
+    } catch (Exception e) {
+      return e.getMessage();
+    }
+  }
+}
